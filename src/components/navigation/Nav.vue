e66727c88fddde36809f5c8873c8deb6a5b42e42
ZiF1R <do-alex03@mail.ru>
2022-02-03 12:25:49 +0300

    Add styles for Nav.vue

--- src/components/navigation/Nav.vue
+++ src/components/navigation/Nav.vue
@@ -10,3 +10,56 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.nav__container {
+  width: calc(20% - 40px);
+  height: calc(100vh - 50px);
+  background: #072125;
+
+  padding: 25px 40px 25px 0;
+}
+
+.nav__logo {
+  display: flex;
+  align-items: center;
+  user-select: none;
+  padding-left: 25px;
+  margin-bottom: 35px;
+}
+
+.nav__logo > img {
+  margin-right: 15px;
+  width: 40px;
+  height: 40px;
+}
+
+a {
+  text-decoration: none;
+}
+
+h1 {
+  font-weight: 600;
+  font-size: 1.8em;
+  color: #42b983;
+  margin: 0;
+
+  transition: color .21s ease;
+  cursor: pointer;
+}
+
+h1:hover {
+  color: #2b9968;
+}
+
+.nav__links {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+}
+
+.link_active {
+  background: var(--primary-color);
+}
+</style>
