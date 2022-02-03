e445dc6e2a582617eeba7627002fa5964c007465
ZiF1R <do-alex03@mail.ru>
2022-02-03 12:25:01 +0300

    Add basic structure of the Nav.vue

---
+++ src/components/navigation/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="nav__container">
+    <div class="nav__logo">
+      <img draggable="false" src="@/assets/cryptocurrency.png" alt="logo" />
+      <router-link to="/">
+        <h1>Cryptoverse</h1>
+      </router-link>
+    </div>
+    <div class="nav__links">
+    </div>
+  </div>
+</template>
