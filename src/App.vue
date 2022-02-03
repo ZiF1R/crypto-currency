d53e807d9705716d61217f4c4e483d951ca004c7
ZiF1R <do-alex03@mail.ru>
2022-02-03 11:40:51 +0300

    Change basic styles and link Nav to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav-bar />
+  <router-view />
 </template>
 
+<script>
+import NavBar from "@/components/navigation/Nav.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+
+  created() {
+    // fetch("https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0", {
+    //   "method": "GET",
+    //   "headers": {
+    //     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+    //     "x-rapidapi-key": "1e65278a3emsh9f2b6c733cedaa9p1fa99cjsn2242707d1d90"
+    //   }
+    // })
+    // .then(response => {
+    //   return response.json();
+    // })
+    // .then(response => {
+    //   console.log(response);
+    // })
+    // .catch(err => {
+    //   console.error(err);
+    // });
+  },
+}
+</script>
+
 <style>
+:root {
+  --primary-color: #42b983;
+  --text-color: #2c3e50;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+
+  font-size: 16px;
+  overflow: hidden;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--text-color);
 }
 </style>
