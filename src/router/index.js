9711f6efc2a326b85f62154f11b81a03e00d5c21
ZiF1R <do-alex03@mail.ru>
2022-02-03 12:21:45 +0300

    Add routes to app

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from "@/views/Home.vue";
+import Cryptocurrencies from "@/views/Cryptocurrencies.vue";
+import Exchanges from "@/views/Exchanges.vue";
+import News from "@/views/News.vue";
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: '@/App.vue'
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/cryptocurrencies',
+    name: 'Cryptocurrencies',
+    component: Cryptocurrencies
+  },
+  {
+    path: '/exchanges',
+    name: 'Exchanges',
+    component: Exchanges
+  },
+  {
+    path: '/news',
+    name: 'News',
+    component: News
+  },
 ]
 
 const router = createRouter({
