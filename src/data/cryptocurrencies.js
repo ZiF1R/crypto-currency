af5001088982b746290ce47719611f939b1de8e2
ZiF1R <do-alex03@mail.ru>
2022-02-06 09:11:43 +0300

    Add getCoin() function and small change of receiving the data

--- src/data/cryptocurrencies.js
+++ src/data/cryptocurrencies.js
@@ -1,44 +1,40 @@
 import { API_KEY } from "@/.env.js";
 
 export const getGlobalStats = async () => {
-  let answer = null;
-  await fetch("https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl", {
+  let answer = await fetch("https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl", {
     "method": "GET",
     "headers": {
       "x-rapidapi-host": "coinranking1.p.rapidapi.com",
       "x-rapidapi-key": API_KEY
     }
-  })
-  .then(response => {
-    return response.json();
-  })
-  .then(response => answer = response)
-  .catch(err => {
-    console.error(err);
-  });
+  }).catch(err => console.error(err));
 
-  return answer;
+  return await answer.json();
 }
 
 export const getCoins = async (limit) => {
   if (typeof limit !== "number" || limit < 0)
     throw new TypeError("The limit must be a number greater than 0!");
 
-  let answer = null;
-  await fetch(`https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=${limit}&offset=0`, {
+  let answer = await fetch(`https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=${limit}&offset=0`, {
     "method": "GET",
     "headers": {
       "x-rapidapi-host": "coinranking1.p.rapidapi.com",
       "x-rapidapi-key": API_KEY
     }
-  })
-  .then(response => {
-    return response.json();
-  })
-  .then(response => answer = response)
-  .catch(err => {
-    console.error(err);
-  });
+  }).catch(err => console.error(err));
 
-  return answer;
+  return await answer.json();
 };
+
+export const getCoin = async (coinUUID) => {
+  let answer = await fetch(`https://coinranking1.p.rapidapi.com/coin/${coinUUID}?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+      "x-rapidapi-key": API_KEY
+    }
+  }).catch(err => console.error(err));
+
+  return await answer.json();
+}
